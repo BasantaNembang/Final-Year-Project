9f2a685ac011841dc1a1d514c886323bc774f625
Basanta Nembang <nembangbasanta66@gmail.com>
2026-02-20 11:35:35 +0545

    checking...

--- Front_End/my-go-to/src/components/heroSection/HeroSection.tsx
+++ Front_End/my-go-to/src/components/heroSection/HeroSection.tsx
@@ -7,7 +7,7 @@ const HeroSection = () => {
       <div className={styles.heroContainer}>
         <div className={styles.info}>
           <div className={styles.headingSection}>
-            <h1>Learn Without Limits  WOW.......................................</h1>
+            <h1>Learn Without Limits</h1>
           </div>
           <div className={styles.infoSection}>
             <p>
