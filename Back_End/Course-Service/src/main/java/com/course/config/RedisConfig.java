4fc73ce3de3d7893b9187a12d8dd1020abe6c41a
Basanta Nembang <nembangbasanta66@gmail.com>
2026-03-11 13:24:23 +0545

    fix the config cache files

--- Back_End/Course-Service/src/main/java/com/course/config/RedisConfig.java
+++ Back_End/Course-Service/src/main/java/com/course/config/RedisConfig.java
@@ -4,6 +4,8 @@ package com.course.config;
 import com.course.dto.ResponseCourseDTO;
 import com.fasterxml.jackson.databind.JavaType;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
@@ -18,40 +20,44 @@ import java.util.List;
 @Configuration
 public class RedisConfig {
 
-    @Bean
-    public RedisCacheManager manager(RedisConnectionFactory redisConnectionFactory){
 
-        //cache for single object
-        RedisCacheConfiguration singleCache  =   RedisCacheConfiguration.defaultCacheConfig()
-                .entryTtl(Duration.ofMinutes(10))
-                .disableCachingNullValues()
-                .serializeValuesWith(RedisSerializationContext.SerializationPair
-                        .fromSerializer(new Jackson2JsonRedisSerializer<>(ResponseCourseDTO.class)));
+        @Bean
+        public RedisCacheManager manager(RedisConnectionFactory redisConnectionFactory) {
 
+            ObjectMapper objectMapper = new ObjectMapper();
+            objectMapper.registerModule(new JavaTimeModule());
+            objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
 
-        //cache for list of object
-        ObjectMapper objectMapper = new ObjectMapper();
-        //JavaType is a type to store  List<T> and so on in a serilzation and deserilization
-        JavaType type = objectMapper.getTypeFactory().constructCollectionType(List.class, ResponseCourseDTO.class);
-        Jackson2JsonRedisSerializer<List<ResponseCourseDTO>> listTYpe = new Jackson2JsonRedisSerializer<>(type);
+            Jackson2JsonRedisSerializer<ResponseCourseDTO> singleSerializer =
+                    new Jackson2JsonRedisSerializer<>(objectMapper, ResponseCourseDTO.class);
 
-        RedisCacheConfiguration listConfiguration  =   RedisCacheConfiguration.defaultCacheConfig()
-                .entryTtl(Duration.ofMinutes(10))
-                .disableCachingNullValues()
-                .serializeValuesWith(RedisSerializationContext.SerializationPair
-                        .fromSerializer(listTYpe));
+            RedisCacheConfiguration singleCache = RedisCacheConfiguration.defaultCacheConfig()
+                    .entryTtl(Duration.ofMinutes(10))
+                    .disableCachingNullValues()
+                    .serializeValuesWith(RedisSerializationContext.SerializationPair
+                            .fromSerializer(singleSerializer));
 
-        return  RedisCacheManager
-                .builder(redisConnectionFactory)
-                .withCacheConfiguration("courses", listConfiguration)
-                .withCacheConfiguration("coursesByCategory", listConfiguration)
-                .withCacheConfiguration("coursesByLevel", listConfiguration)
-                .withCacheConfiguration("coursesByPrice", listConfiguration)
-                .withCacheConfiguration("course", singleCache)
-                .cacheDefaults(singleCache)
-                .build();
-    }
+            JavaType type = objectMapper.getTypeFactory()
+                    .constructCollectionType(List.class, ResponseCourseDTO.class);
+            Jackson2JsonRedisSerializer<List<ResponseCourseDTO>> listSerializer =
+                    new Jackson2JsonRedisSerializer<>(objectMapper, type);
 
+            RedisCacheConfiguration listConfiguration = RedisCacheConfiguration.defaultCacheConfig()
+                    .entryTtl(Duration.ofMinutes(10))
+                    .disableCachingNullValues()
+                    .serializeValuesWith(RedisSerializationContext.SerializationPair
+                            .fromSerializer(listSerializer));
+
+            return RedisCacheManager
+                    .builder(redisConnectionFactory)
+                    .withCacheConfiguration("courses", listConfiguration)
+                    .withCacheConfiguration("coursesByCategory", listConfiguration)
+                    .withCacheConfiguration("coursesByLevel", listConfiguration)
+                    .withCacheConfiguration("coursesByPrice", listConfiguration)
+                    .withCacheConfiguration("course", singleCache)
+                    .cacheDefaults(singleCache)
+                    .build();
+        }
 
 
 
